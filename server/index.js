577f98a3b2df201ecc170c9aefb5788f9f4a9313
Lazar Dudic <lazar.dudic@yahoo.com>
2021-06-09 22:39:59 +0200

    set up

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.urlencoded({limit: "30mb", extended: true}))
+app.use(express.json({limit: "30mb", extended: true})) 
+app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://Lazar90:Lazar90@cluster0.0dlvr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, (console.log(`Server is running om port: ${PORT}`))))
+    .catch((err) => console.log(err.message))
+
+mongoose.set('useFindAndModify', false);
