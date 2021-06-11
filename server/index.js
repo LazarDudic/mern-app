be747009aece58e0095d48d353269b6481ecc40c
Lazar Dudic <lazar.dudic@yahoo.com>
2021-06-11 20:32:45 +0200

    Created Router, Controller, Model

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
+import postRoutes from './routes/posts.js'
+
 const app = express()
+
+app.use('/posts', postRoutes)
 
 app.use(express.urlencoded({limit: "30mb", extended: true}))
 app.use(express.json({limit: "30mb", extended: true})) 
