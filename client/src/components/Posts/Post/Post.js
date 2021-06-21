0ac86e1fd099515a054e35fe9861fb3274ee0dcd
Lazar Dudic <lazar.dudic@yahoo.com>
2021-06-21 21:15:58 +0200

    Like and delete

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,9 +5,13 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
 import useStyles from './styles'
+import { useDispatch } from 'react-redux'
+
+import { deletePost, likePost } from '../../../actions/posts'
 
 function Post({ post, setCurrentId }) {
     const classes = useStyles()
+    const dispatch = useDispatch()
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title}></CardMedia>
@@ -28,13 +32,13 @@ function Post({ post, setCurrentId }) {
             <Typography  variant="h5" gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => {}}>
-                    <DeleteIcon fontSize="small" />
+                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
+                    <ThumbUpAltIcon fontSize="small" />
                     Like
                     {post.likeCount}
                 </Button>
-                <Button size="small" color="primary" onClick={() => {}}>
-                    <ThumbUpAltIcon fontSize="small" />
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
+                    <DeleteIcon fontSize="small" />
                     Delete
                 </Button>
             </CardActions>
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import {getPosts, createPost, updatePost } from '../controllers/postsController.js'
+import {getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/postsController.js'
 
 router.get('/', getPosts)
 router.post('/', createPost)
 router.patch('/:id', updatePost)
+router.delete('/:id', deletePost)
+router.patch('/:id/like', likePost)
 
 export default router
